b0044c1feaf071ed4ba0432ce4c4957df27bcc5a
Raccoon Auto History <raccoon@local>
2026-05-15 14:08:11 +0200

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.100.159:/home/pi/programs/322a6cb2-b54d-4ad2-bb55-14d157403ae7
    Summary: 4 files (+0 ~4 -0)
    
    Changed files:
    - M src/hardware/__pycache__/defs.cpython-313.pyc
    - M src/hardware/__pycache__/robot.cpython-313.pyc
    - M src/missions/m000_setup_mission.py
    - M src/missions/m010_grab_first_poms_mission.py

--- src/missions/m000_setup_mission.py
+++ src/missions/m000_setup_mission.py
@@ -8,31 +8,31 @@ class M000SetupMission(SetupMission):
     def sequence(self) -> Sequential:
         setup_time = 120
         return seq([
-            pause_setup_timer(),
-            fully_disable_servos(),
-            wait_for_button("move servos into starting position"),
-            start_setup_timer(),  # countdown begins here, full duration
-            parallel(
-                Defs.pom_grab.start(),
-                Defs.shild.down(),
-                Defs.shild_graber.closed(),
-            ),
-
-            parallel(
-                Defs.pom_arm.start(100),
-            ),
-
-            Defs.shild.up(),
-
-
-            #auto_tune(
-            #    vel_axes=["vy"],
-            #    tune_motion=False,
-            #    characterize_axes=["lateral"]
-            #),
+            # pause_setup_timer(),
+            # fully_disable_servos(),
+            # wait_for_button("move servos into starting position"),
+            # start_setup_timer(),  # countdown begins here, full duration
+            # parallel(
+            #     Defs.pom_grab.start(),
+            #     Defs.shild.down(),
+            #     Defs.shild_graber.closed(),
+            # ),
+            #
+            # parallel(
+            #     Defs.pom_arm.start(100),
+            # ),
+            #
+            # Defs.shild.up(),
+            #
+            #
+            # #auto_tune(
+            # #    vel_axes=["vy"],
+            # #    tune_motion=False,
+            # #    characterize_axes=["lateral"]
+            # #),
             calibrate(distance_cm=70,
                       calibration_sets=["default", "upper"],
                       ),
-
-            fully_disable_servos(),
+            #
+            # fully_disable_servos(),
         ])
